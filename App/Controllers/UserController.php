b3b6e1eec29c1676dc2c714641bfc260e0ad2960
Yozko13 <yozko13@gmail.com>
2021-09-24 17:12:15 +0300

    I completed with the cro operations with insert, update and delete.

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -8,7 +8,9 @@ use App\System\Controller;
 class UserController extends Controller
 {
 
-
+    /**
+     * @throws \Exception
+     */
     public function index()
     {
         $city = [];
@@ -20,18 +22,52 @@ class UserController extends Controller
         ]);
     }
 
+    /**
+     * @throws \Exception
+     */
     public function profile()
     {
         $this->showView('user/profile', ['profile' => 'Profile']);
     }
 
+    /**
+     * @throws \Exception
+     */
     public function profileSettings()
     {
         $this->showView('user/profile-settings', ['settings' => 'Profile Settings']);
     }
 
+    /**
+     * @throws \Exception
+     */
     public function addUser()
     {
+        $user = new User();
+        $user->insert(['name' => 'testmest', 'city' => 'dimitrovgrad']);
+
         $this->showView('user/add-user', ['addUser' => 'Add User']);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function updateUser()
+    {
+        $user = new User();
+        $user->update(['name' => 'test update 1', 'city' => 'test city 1', 'id' => 7], ['id' => 7]);
+
+        $this->showView('user/update-user', ['updateUser' => 'Update User']);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function deleteUser()
+    {
+        $user = new User();
+        $user->delete(['id' => 6]);
+
+        $this->showView('user/delete-user', ['deleteUser' => 'Delete User']);
     }
 }
--- App/System/AbstractModel.php
+++ App/System/AbstractModel.php
@@ -30,16 +30,57 @@ abstract class AbstractModel
 
     public function insert($data)
     {
-        $tableName = $this->tableName;
+        $columns     = implode(', ', array_keys($data));
+        $dataValues  = array_values($data);
+        $valueString = implode(',', array_fill(0, count($data), '?'));
+
+        $sql        = "INSERT INTO {$this->tableName} ({$columns}) VALUES ({$valueString})";
+        $statement  = $this->pdo->prepare($sql);
+        $statement->execute($dataValues);
     }
 
-    public function update()
+    /**
+     * @param $data
+     * @param $whereClauses
+     * @return int
+     */
+    public function update($data, $whereClauses): int
     {
+        $setSet   = implode(', ', $this->transformToSet($data));
+        $whereSet = implode(' AND ', $this->transformToSet($whereClauses));
 
+        $sql       = "UPDATE {$this->tableName} SET {$setSet} WHERE {$whereSet}";
+        $statement = $this->pdo->prepare($sql);
+
+        $executeParams = array_merge(array_values($data), array_values($whereClauses));
+        $statement->execute($executeParams);
+
+        return $statement->rowCount();
     }
 
-    public function delete()
+    /**
+     * @param $data
+     * @return int
+     */
+    public function delete($data): int
     {
+        $whereSet  = implode(' AND ', $this->transformToSet($data));
+        $statement = $this->pdo->prepare("DELETE FROM {$this->tableName} WHERE {$whereSet}");
+        $statement->execute(array_values($data));
 
+        return $statement->rowCount();
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    private function transformToSet(array $data): array
+    {
+        $result = [];
+        foreach ($data as $column => $value) {
+            $result[] = "$column = ?";
+        }
+        return $result;
     }
 }
